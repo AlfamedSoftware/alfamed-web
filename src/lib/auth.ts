4ad44ee1ecfb3af615b5952c00a2ce5df3ebd046
Vinicius Engelmann <vinicius.engelmann@gmail.com>
2026-02-12 22:14:59 -0300

    fea: 2fa plugin client implement

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
 import { createAuthClient } from "better-auth/client";
+import { twoFactorClient } from "better-auth/client/plugins"
 
 export const auth = createAuthClient({
     baseURL: "http://localhost:3333",
+    plugins: [twoFactorClient()],
 })
